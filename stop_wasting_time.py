87ef7b8301d34040f96869028c5f385fe4ddc512
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2024-02-12 16:59:34 +0000

    Add files via upload

--- stop_wasting_time.py
+++ stop_wasting_time.py
@@ -1,18 +1,36 @@
+
 import os
+import time
 
-hosts_path = r"C:\Windows\System32\drivers\etc\hosts" # Use r"/etc/hosts" for macOS and Linux
+# Path to the hosts file
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts" if os.name == 'nt' else "/etc/hosts"
 redirect = "127.0.0.1"
+# Websites you want to control
+websites = ["www.facebook.com", "facebook.com", "www.onet.pl", "onet.pl", "www.reddit.com", "reddit.com", "www.instagram.com", "instagram.com"]
 
+def toggle_websites_blocking():
+    with open(hosts_path, 'r+') as file:
+        content = file.readlines()
+        file.seek(0)
+        blocked = False
+        for line in content:
+            if any(website in line for website in websites):
+                blocked = True
+                break
+        
+        if blocked:
+            # If websites are found in the file, remove them (unblock)
+            new_content = [line for line in content if not any(website in line for website in websites)]
+            file.writelines(new_content)
+            print("Websites have been unblocked.")
+        else:
+            # If websites are not found in the file, block them
+            file.writelines(content)
+            for website in websites:
+                file.write(f"{redirect} {website}\n")
+            print("Websites have been blocked.")
+        file.truncate()
 
-websites_to_block = ["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.com", "www.onet.pl"]
+toggle_websites_blocking()
 
-
-def block_websites():
-    with open(hosts_path, 'r+') as hostsfile:
-        hosts_content = hostsfile.read()
-        for website in websites_to_block:
-            if website not in hosts_content:
-                hostsfile.write(redirect + " " + website + "\n")
-    print("Websites have been blocked.")
-
-block_websites()
+time.sleep(3)
